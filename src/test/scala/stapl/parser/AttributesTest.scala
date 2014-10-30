0ecd64fa152be5d7159a6d23402cc85c39aa3056
Jasper Moeys <jaspermoeys@hotmail.com>
2014-10-30 16:07:02 +0100

    add test for AttributesParser

---
+++ src/test/scala/stapl/parser/AttributesTest.scala
@@ -0,0 +1,76 @@
+/**
+ *    Copyright 2014 KU Leuven Research and Developement - iMinds - Distrinet
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Administrative Contact: dnet-project-office@cs.kuleuven.be
+ *    Technical Contact: maarten.decat@cs.kuleuven.be
+ *    Author: maarten.decat@cs.kuleuven.be
+ */
+package stapl.parser
+
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import stapl.core.pdp.PDP
+import stapl.core.pdp.AttributeFinder
+import org.junit.Assert._
+import org.scalatest.junit.AssertionsForJUnit
+import stapl.core._
+import scala.io.Source
+import scala.util.Success
+import EdocsAttributes.action
+import EdocsAttributes.environment
+import EdocsAttributes.resource
+import EdocsAttributes.subject
+import org.parboiled2.ParserInput.apply
+import scala.util.Failure
+import org.parboiled2.ParseError
+
+object AttributesTest {
+  
+  @BeforeClass def setup() {
+    // nothing to do
+  }
+}
+
+class AttributesTest extends AssertionsForJUnit {
+
+  @Before def setup() {
+    // nothing to do
+  }
+
+  @Test def testParseAttributes() {
+    val parser = new AttributesParser(
+      """
+      subject.name = SimpleAttribute(String)
+      resource.names = ListAttribute(String)
+      environment.nrnames = SimpleAttribute (  Number )
+      action.day = SimpleAttribute(DayDuration)
+      """)
+    val attributes = parser.AttributeDefs.run() match {
+      case Success(result) => result
+      case Failure(e: ParseError) => print(parser.formatError(e)); sys.error("wrong input")
+      case Failure(e) => e.printStackTrace(); sys.error("wrong input")
+    }
+    
+    
+    assert(
+        attributes 
+          === 
+        Seq(SimpleAttribute(SUBJECT, "name", String), ListAttribute(RESOURCE, "names", String), 
+            SimpleAttribute(ENVIRONMENT, "nrnames", Number), SimpleAttribute(ACTION, "day", DayDuration))
+    )
+  }
+
+}
